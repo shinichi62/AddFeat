28de9654120b0dac7dd3bf4ee25d6070322dfd5d
shinichi yamaguchi <shinichi62@gmail.com>
2017-08-16 00:45:53 +0900

    Clean code

--- options.js
+++ options.js
@@ -1,14 +1,11 @@
-// Initial settings
 $(function(){
   chrome.storage.sync.get({prefix: "feat/", description: ""}, function(items) {
     $("#prefix").val(items.prefix);
     $("#description").val(items.description);
   });
-});
-
-// Saves options to chrome.storage
-$("#save").click(function () {
-  chrome.storage.sync.set({'prefix': $("#prefix").val(), 'description': $("#description").val()}, function() {
-    $("#status").text("Settings saved.");
-  });
+  $("#save").click(function () {
+    chrome.storage.sync.set({'prefix': $("#prefix").val(), 'description': $("#description").val()}, function() {
+      $("#status").text("Settings saved.");
+    });
+  });  
 });
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 $(function(){
   chrome.storage.sync.get({prefix: "feat/", description: ""}, function(items) {
-    if ($("#id_branch_name").val() != undefined) {
-      if ($("#id_branch_name").val().indexOf(items.prefix) === -1) {
-        $("#id_branch_name").val(items.prefix + $("#id_branch_name").val());
-      }
+    if (jQuery.type($("#id_branch_name").val()) !== "undefined" && $("#id_branch_name").val().indexOf(items.prefix) === -1) {
+      $("#id_branch_name").val(items.prefix + $("#id_branch_name").val());
     }
-    if ($("#id_description").val() != undefined && $("#id_description").val() == "") {
+    if (jQuery.type($("#id_description").val()) !== "undefined"  && $("#id_description").val() === "") {
       $("#id_description").val(items.description)
     }
   });
