74a24ce055cc29db4e61328685e35c75c8994a9c
shinichi yamaguchi <shinichi62@gmail.com>
2017-08-15 23:19:37 +0900

    Set default pull request description
    * プルリクエストの雛形を設定できるよう修正

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(function(){
-  chrome.storage.sync.get({prefix: "feat/"}, function(items) {
-    if ($("#id_branch_name").val().indexOf(items.prefix) === -1) {
-      $("#id_branch_name").val(items.prefix + $("#id_branch_name").val());
+  chrome.storage.sync.get({prefix: "feat/", description: ""}, function(items) {
+    if ($("#id_branch_name").val() != undefined) {
+      if ($("#id_branch_name").val().indexOf(items.prefix) === -1) {
+        $("#id_branch_name").val(items.prefix + $("#id_branch_name").val());
+      }
+    }
+    if ($("#id_description").val() != undefined && $("#id_description").val() == "") {
+      $("#id_description").val(items.description)
     }
   });
 });
